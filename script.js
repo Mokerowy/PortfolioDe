d1e26d5fda8e4302f7584204ba68b7776ab6cbfe
Mokerowy <123027494+Mokerowy@users.noreply.github.com>
2025-09-16 21:13:55 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
+
+    if (targetElement) {
+      window.scrollTo({
+        top: targetElement.offsetTop,
+        behavior: "smooth",
+      });
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const video = document.getElementById("background-video");
+  const dustContainer = document.querySelector(".dust");
+
+  video.addEventListener("ended", () => {
+    dustContainer.style.opacity = "0";
+
+    setTimeout(() => {
+      dustContainer.remove();
+    }, 1000);
+  });
+});
